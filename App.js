f804e25bdd938c4e154566ca124a7d53dc9e2493
aeroluxx <aeroluxx@ya.ru>
2019-01-27 17:56:36 +0300

    New Component Layout
    Add a new Component 'Layout'

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import {StyleSheet, Text, View, ScrollView} from 'react-native'
-import { Header, ImageCard } from './src/components'
+import {Text, View} from 'react-native'
+import { Header, ImageCard, Layout } from './src/components'
 import { w, h } from './src/components/Constants'
 
 const url='https://raw.githubusercontent.com/react-native-village/react-native-init/master/stargate/stargate.json'
@@ -25,29 +25,15 @@ export default class App extends Component {
 
   render() {
     const {title, data} = this.state
-    const {screenStyle} = styles
     return (
         <View>
-          <ScrollView>
             <Header title={this.state.title}/>
-              <View style={screenStyle}>
+              <Layout>
                 {data.map( item => (
                   <ImageCard data={item} key = {item.id} />
                 ))}
-              </View>
-            </ScrollView>
+              </Layout>
         </View>
     )
   }
 }
-
-const styles=StyleSheet.create({
-  screenStyle: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    flexShrink: 2,
-    justifyContent: 'space-around',
-    marginBottom: 100
-    }
-  }
-)
---
+++ src/components/Layout.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {StyleSheet, Text, View, ScrollView} from 'react-native'
+
+const Layout = props => {
+  const { screenStyle } = styles
+  return (
+  <ScrollView>
+    <View style={screenStyle}>
+      {props.children}
+    </View>
+  </ScrollView>
+  )
+}
+
+const styles=StyleSheet.create({
+  screenStyle: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexShrink: 2,
+    justifyContent: 'space-around',
+    marginBottom: 150
+    }
+  }
+)
+
+export {Layout}
